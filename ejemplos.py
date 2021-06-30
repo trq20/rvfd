f13bd32dfd343d6b46008361e3c9a0cd09742d58
Fabrizio Carlassara <53102151+carlassaraf@users.noreply.github.com>
2021-06-30 15:21:09 -0300

    Add files via upload

---
+++ ejemplos.py
@@ -0,0 +1,32 @@
+# LED Toggle
+'''
+from machine import Pin
+from time import sleep
+
+led = Pin(2, Pin.OUT)
+
+while True:
+    led.value(not led.value())
+    sleep(1)
+'''
+
+# ADC 
+'''
+from machine import Pin, ADC
+from time import sleep
+
+v = Pin(25, Pin.OUT, value = 1)
+adc = ADC(Pin(36))
+
+while True:
+
+    print("El valor del LDR es {}".format(adc.read()))
+    sleep(1)
+'''
+# PWM
+
+from machine import Pin, PWM
+from time import sleep
+
+pwm = PWM(Pin(14), freq = 5000, duty = 100)
+    
